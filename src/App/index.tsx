a493e70955f1511761b853adc7737a0edfc372c4
Josue Vargas <cevargas@est.ecotec.edu.ec>
2022-11-22 16:22:28 -0500

    Composicion y colocacion del estado

--- src/App/index.tsx
+++ src/App/index.tsx
@@ -1,13 +1,13 @@
-import React from 'react';
-import './App.css';
-import { Todo } from '../models/todo';
+import React from "react";
+import "./App.css";
+import { Todo } from "../models/todo";
 
-// PRIME NG 
-import "primereact/resources/themes/lara-light-indigo/theme.css";  //theme
-import "primereact/resources/primereact.min.css";                  //core css
-import "primeicons/primeicons.css";     
-import { AppUI } from './AppUI';
-import { TodoProvider } from '../TodoContext';
+// PRIME NG
+import "primereact/resources/themes/lara-light-indigo/theme.css"; //theme
+import "primereact/resources/primereact.min.css"; //core css
+import "primeicons/primeicons.css";
+import { AppUI } from "./AppUI";
+import { TodoProvider } from "../TodoContext";
 
 // const defaultTodos: Todo[] = [
 //   {text: 'Cortar cebolla', completed: true},
@@ -16,12 +16,48 @@ import { TodoProvider } from '../TodoContext';
 //   {text: 'Llorar con la ssss', completed: false},
 // ]
 
+// export const App = () => {
+//   return (
+//     <TodoProvider>
+//           <AppUI/>
+//     </TodoProvider>
+//   );
+// }
+
 export const App = () => {
+  const [todos, setTodos] = React.useState("estado compartido");
+
   return (
-    <TodoProvider>
-          <AppUI/>
-    </TodoProvider>
+    <React.Fragment>
+      <TodoHeader>
+        <TodoCounter />
+        <TodoSearch />
+      </TodoHeader>
+      <TodoList>
+        <TodoItem state={todos} />
+      </TodoList>
+    </React.Fragment>
   );
-}
+};
+
+export const TodoHeader = ({ children }: any) => {
+  return <header>{children}</header>;
+};
+
+export const TodoList = ({ children }: any) => {
+  return <section className="TodoList-container">{children}</section>;
+};
+
+export const TodoCounter = () => {
+  return <p>TodoCounter</p>;
+};
+
+export const TodoSearch = () => {
+  return <p>TodoSearch</p>;
+};
+
+export const TodoItem = ({ state }: any) => {
+  return <p>TodoItem: {state}</p>;
+};
 
 export default App;
